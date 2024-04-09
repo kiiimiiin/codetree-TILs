6825145357e4fedefb0844d4c7d3925aa44336a1
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-04-09 12:10:55 +0900

    Title: 포탑 부수기, Time: 76ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240409/포탑 부수기/destroy-the-turret.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <tuple>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#define X first 
+#define Y second
+using namespace std;
+pair<int, int> board[12][12]; // 공격력, 최근 공격한 turn
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 }; // 우하좌상
+int n, m, k;
+int t; // turn
+int oneFlag;
+
+bool cmp(tuple<int, int, int, int>& t1, tuple<int, int, int, int>& t2) {
+	if (get<0>(t1) != get<0>(t2)) return get<0>(t1) < get<0>(t2);
+	if (get<1>(t1) != get<1>(t2)) return get<1>(t1) > get<1>(t2);
+	if (get<2>(t1) != get<2>(t2)) return get<2>(t1) > get<2>(t2);
+	return get<3>(t1) > get<3>(t2);
+}
+
+tuple<int, int, int, int> ChooseTarget() { // 가장 약한 공격자, 강한 공격대상 선정
+	vector<tuple<int, int, int, int>> infos; // 공격력, 공격 turn, 행열합,열
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (board[i][j].first == 0) continue;
+			infos.push_back({ board[i][j].first, board[i][j].second
+				, i + j, j });
+		}
+	}
+	
+
+	sort(infos.begin(), infos.end(), cmp);
+	int ax, ay, sum;
+	tie(ignore, ignore, sum, ay) = infos.front();
+	ax = sum - ay;
+
+	int tx, ty;
+	tie(ignore, ignore, sum, ty) = infos.back();
+	tx = sum - ty;
+	return { ax, ay , tx, ty };
+}
+
+
+pair<int, int> ProcessOOB(int nx, int ny) {
+	if (nx < 0) nx = n + nx;
+	if (nx >= n) nx = nx - n;
+	if (ny < 0) ny = m + ny;
+	if (ny >= m) ny = ny - m;
+
+	return { nx, ny };
+}
+
+bool Laser(int ax, int ay, int tx, int ty) {
+	queue<pair<int, int>> q;
+	int dist[12][12] = {};
+	for (int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
+
+	q.push({ tx,ty });
+	dist[tx][ty] = 0;
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+			tie(nx, ny) = ProcessOOB(nx, ny);
+			if (board[nx][ny].first == 0 || dist[nx][ny] >= 0)
+				continue;
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			q.push({ nx,ny });
+		}
+	} // 타겟부터 공격자까지 거리값 계산
+
+	if (dist[ax][ay] == -1) return false; // 포탑공격
+	stack<pair<int, int>> s;
+	int route[12][12] = {};
+	s.push({ ax, ay });
+	while (route[tx][ty] != 1) {
+		auto cur = s.top(); s.pop();
+		route[cur.X][cur.Y] = 1;
+		for (int dir = 3; dir >= 0; dir--) { // 상좌하우
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+			tie(nx, ny) = ProcessOOB(nx, ny);
+			if (dist[nx][ny] == -1 || route[nx][ny] == 1)
+				continue;
+			if (dist[nx][ny] == dist[cur.X][cur.Y] - 1) {
+				s.push({ nx,ny });
+			}
+		}
+	} // 공격자부터 타겟까지 최단루트 탐색
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i == ax && j == ay) continue;
+			if (route[i][j] == 1) {
+				if (i == tx && j == ty)
+					board[i][j].first -= board[ax][ay].first;
+				else
+					board[i][j].first -= board[ax][ay].first / 2;
+
+				if (board[i][j].first < 0)
+					board[i][j].first = 0;
+			}
+		}
+	}
+}
+
+void Bomb(int ax, int ay, int tx, int ty) {
+	int bx[9] = { 0,-1,-1,-1,0, 0,1,1,1 };
+	int by[9] = { 0,-1,0,1,-1,1,-1,0,1 };
+
+	for (int dir = 0; dir < 9; dir++) {
+		int x = tx + bx[dir];
+		int y = ty + by[dir];
+		tie(x, y) = ProcessOOB(x, y);
+		if (x == ax && y == ay) continue;
+		if (board[x][y].first == 0) continue;
+		if (dir == 0)
+			board[x][y].first -= board[ax][ay].first;
+		else
+			board[x][y].first -= board[ax][ay].first / 2;
+
+		if (board[x][y].first < 0)
+			board[x][y].first = 0;
+	}
+
+}
+
+void Copy(pair<int, int> desc[12][12], pair<int, int> src[12][12]) {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			desc[i][j] = src[i][j];
+}
+
+void Attack(int ax, int ay, int tx, int ty) {
+
+	pair<int, int> cBoard[12][12];
+	Copy(cBoard, board);
+
+	// 공격자 공격력 Up, 공격 turn Update
+	board[ax][ay].first += n + m;
+	board[ax][ay].second++;
+
+	// 포탑공격 
+	if (!Laser(ax, ay, tx, ty))
+		Bomb(ax, ay, tx, ty);
+
+	// 포탑정비
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (board[i][j].first == 0) continue;
+			if (i == ax && j == ay) continue;
+			cnt++;
+			if (board[i][j].first == cBoard[i][j].first
+				&& board[i][j].second == cBoard[i][j].second)
+				board[i][j].first++;
+		}
+	}
+
+	if (cnt == 1) oneFlag = 1;
+}
+
+int main(void) {
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			int stat;
+			cin >> stat;
+			board[i][j].first = stat;
+			board[i][j].second = t; // 모든 포탑의 최근 공격turn은 0
+		}
+	}
+
+	for (t = 1; t <= k; t++) {
+		int ax, ay, tx, ty;
+
+		tie(ax, ay, tx, ty) = ChooseTarget();
+
+		Attack(ax, ay, tx, ty);
+		if (oneFlag) break;
+	}
+
+	// 가장 강한 포탑 공격력 출력
+	int tx, ty;
+	tie(ignore, ignore, tx, ty) = ChooseTarget();
+	cout << board[tx][ty].first;
+}
