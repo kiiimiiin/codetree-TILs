0abe0d53f332d43079120bea35fca2a8a2a97f86
kiiimiiin <121804582+kiiimiiin@users.noreply.github.com>
2024-08-20 15:15:47 +0900

    Title: 팩맨, Time: 135ms, Memory: 66MB, Status: Passed - CodeTree

--- 240820/팩맨/pacman.cpp
+++ 240820/팩맨/pacman.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <tuple>
 #define X first
 #define Y second
 using namespace std;
-
+struct Monster {
+	int r, c, d;
+	int idx;
+	bool isLived = true;
+};
 
 struct Pack {
 	int r, c;
@@ -12,12 +15,16 @@ struct Pack {
 
 const int dx[8] = { -1,-1,0,1,1,1,0,-1 };
 const int dy[8] = { 0, -1,-1,-1,0, 1,1,1 };
-
 Pack pack;
-vector<int> board[5][5]; // 몬스터 보드 ( 방향 )
-vector<int> copied[5][5];
-int demon[5][5];
-int m, t;
+vector<Monster> monster;
+vector<Monster> copied;
+vector<int> board[5][5]; // 인덱스 보드
+int demon[5][5]; // 시체 보드
+int m, t; 
+int mx;
+int mxDir[3], packDir[3];
+int passedAlready[5][5]; 
+vector<pair<int, int>> eattenPos;
 
 
 bool OOB(int x, int y) {
@@ -29,35 +36,34 @@ bool ExistPack(int x, int y) {
 }
 
 void CopyMonster() {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			copied[i][j] = board[i][j];
-		}
+	for (int i = 0; i < monster.size(); i++) {
+		if (!monster[i].isLived) continue;
+		copied.push_back(monster[i]);
 	}
 }
 
 
 void MoveMonster() {
 	vector<int> temp[5][5];
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (board[i][j].empty()) continue;
-			for (auto md : board[i][j]) {
-				int dd;
-				for (dd = 0; dd < 8; dd++) {
-					int nd = (md + dd) % 8;
-					int nx = i + dx[nd];
-					int ny = j + dy[nd];
-					if (OOB(nx, ny) || demon[nx][ny] || ExistPack(nx, ny))
-						continue;
-					temp[nx][ny].push_back(nd);
-					break;
-				}
-				if (dd == 8)
-					temp[i][j].push_back(md);
-			}
+	int dd; 
+	for (int i = 0; i < monster.size(); i++) {
+		if (!monster[i].isLived) continue;
+		
+		for (dd = 0; dd < 8; dd++) {
+			int nd = ( monster[i].d + dd ) % 8;
+			int nx = monster[i].r + dx[nd];
+			int ny = monster[i].c + dy[nd];
+			if (OOB(nx, ny) || demon[nx][ny] || ExistPack(nx, ny)) 
+				continue;
+			temp[nx][ny].push_back(i);
+			monster[i].r = nx;
+			monster[i].c = ny; 
+			monster[i].d = nd;
+			break;
 		}
+
+		if (dd == 8)
+			temp[monster[i].r][monster[i].c].push_back(i);
 	}
 
 	for (int i = 0; i < 4; i++) {
@@ -68,69 +74,55 @@ void MoveMonster() {
 }
 
 
-
-void MovePack() {
-	/*
-		상 좌 하 우
-		0 1 2 3
-	*/
-	tuple<int, int, int, int> mx = { -1,0,0,0};
-	int mxDir[3];
-	
-	// 1. 최대 섭취 방향 구함
-	for (int cse = 0; cse < (1 << 6); cse++) {
-		int brute = cse;
-		int dir[3];
-		bool passed[5][5] = {};
-		
-		// 방향정보와 우선순위 확인
-		for (int i = 0; i < 3; i++) {
-			dir[i] = brute % 4;
-			brute /= 4;
+void dfs(int k, int pr, int pc, int eatten) {
+	if (k >= 3) {
+		// 최대 초기화
+		if (eatten > mx) {
+			mx = eatten;
+			for (int i = 0; i < 3; i++) 
+				mxDir[i] = packDir[i];
 		}
-		
-		
-		// 해당 방향으로 
-		int curX = pack.r;
-		int curY = pack.c; 
-		int eatten = 0;
-		bool isSkipped = false;
-		for (int i = 0; i < 3; i++) {
-			int nx = curX + dx[dir[i] * 2];
-			int ny = curY + dy[dir[i] * 2];
-			if (OOB(nx, ny)) {
-				isSkipped = true;
-				break;
-			}
-			
-			if(!passed[nx][ny])
-				eatten += board[nx][ny].size();
-			passed[nx][ny] = true;
-
-			curX = nx;
-			curY = ny;
-		}
-		if (isSkipped) continue;
-		if (make_tuple(eatten, -dir[0], -dir[1], -dir[2]) > mx){
-			mx = make_tuple(eatten, -dir[0], -dir[1], -dir[2]);
-		}
+		return;
 	}
 
+	for (int dir : {0, 2, 4, 6}) {
+		int nx = pr + dx[dir];
+		int ny = pc + dy[dir];
+		int ed;
+		if (OOB(nx, ny)) continue;
+		packDir[k] = dir;
 
-	tie(ignore, mxDir[0], mxDir[1], mxDir[2]) = mx;
-	for (int i = 0; i < 3; i++) mxDir[i] = -mxDir[i];
+		if (passedAlready[nx][ny])
+			ed = 0;
+		else
+			ed = board[nx][ny].size();
 
-	// 2. 최대섭취에 따라 실제 섭취
+		passedAlready[nx][ny]++; // *** 
 
-	for (int i = 0; i < 3; i++) {
-		int nx = pack.r + dx[mxDir[i] * 2];
-		int ny = pack.c + dy[mxDir[i] * 2];
+		dfs(k + 1, nx, ny, eatten + ed);
+
+		passedAlready[nx][ny]--;
 		
-		if (!board[nx][ny].empty()) {
-			board[nx][ny].clear();
+	}
+}
+
+void MovePack() {
+	mx = -0x7f7f7f7f;
+	fill(mxDir, mxDir + 3, -1);
+	dfs(0, pack.r, pack.c, 0);
+
+	//if (mx == -0x7f7f7f7f) { exit(0); }
+	for (int i = 0; i < 3; i++) {
+		int nx = pack.r + dx[mxDir[i]];
+		int ny = pack.c + dy[mxDir[i]];
+		
+		if (!board[nx][ny].empty())
 			demon[nx][ny] = 3;
-		}
-			
+		
+		for (auto idx : board[nx][ny])
+			monster[idx].isLived = false;
+
+		board[nx][ny].clear();
 		pack.r = nx;
 		pack.c = ny;
 	}
@@ -144,14 +136,11 @@ void RemoveDemon() {
 }
 
 void WakeUp() {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (copied[i][j].empty()) continue;
-			while (!copied[i][j].empty()) {
-				board[i][j].push_back(copied[i][j].back());
-				copied[i][j].pop_back();
-			}
-		}
+	while (!copied.empty()) {
+		Monster cm = copied.back();
+		monster.push_back(cm);
+		board[cm.r][cm.c].push_back(monster.size() - 1);
+		copied.pop_back();
 	}
 }
 
@@ -159,9 +148,11 @@ int getAns() {
 	int ret = 0;
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
+			if (board[i][j].empty()) continue;
 			ret += board[i][j].size();
 		}
 	}
+
 	return ret;
 }
 
@@ -174,10 +165,11 @@ int main() {
 	pack.r--; pack.c--;
 
 	for (int i = 0; i < m; i++) {
-		int r, c, d;
-		cin >> r >> c >> d;
-		r--; c--; d--;
-		board[r][c].push_back(d);
+		Monster ms; 
+		cin >> ms.r >> ms.c >> ms.d;
+		ms.r--; ms.c--; ms.d--;
+		monster.push_back(ms);
+		board[ms.r][ms.c].push_back(i);
 	}
 
 	int ans = 0;
